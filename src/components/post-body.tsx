3feb693ef010312eeb90c7fb3fff38f36a86dc5a
chahyunwoo <chahyunwoobi@gmail.com>
2025-03-28 17:08:38 +0900

    code styling

--- src/components/post-body.tsx
+++ src/components/post-body.tsx
@@ -1,11 +1,37 @@
 import { Post } from "@/types";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import { compileMDX } from "next-mdx-remote/rsc";
+import rehypePrettyCode, { Options } from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkBreaks from "remark-breaks";
+import remarkGfm from "remark-gfm";
+
+const prettyCodeOptions: Options = {
+  theme: {
+    dark: "ayu-dark",
+    light: "snazzy-light",
+  },
+};
 
 interface PostBodyProps {
   post: Post;
 }
 
 export default function PostBody({ post }: PostBodyProps) {
-  return <MDXRemote source={post.content} />;
+  return (
+    <MDXRemote
+      source={post.content}
+      options={{
+        mdxOptions: {
+          remarkPlugins: [remarkGfm, remarkBreaks],
+          rehypePlugins: [
+            rehypeSlug,
+            [
+              rehypePrettyCode,
+              { theme: { dark: "ayu-dark", light: "snazzy-light" } },
+            ],
+          ],
+        },
+      }}
+    />
+  );
 }
